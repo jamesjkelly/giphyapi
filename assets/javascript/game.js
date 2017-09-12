f1b864bb8853c6e53a3f809d0d20953950f1b342
jamesjkelly <jkellynjus@hotmail.com>
2017-09-11 23:55:49 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,20 @@
 $("button").click(function(){
     $("#main").css('display','block');
 });
+
+
+    var mins = 1;   
+    var secs = mins * 60;
+    var currentSeconds = 0;
+    var currentMinutes = 0;
+    
+    setTimeout(Decrement,1000); 
+
+    function Decrement() {
+        currentMinutes = Math.floor(secs / 60);
+        currentSeconds = secs % 60;
+        if(currentSeconds <= 9) currentSeconds = "0" + currentSeconds;
+        secs--;
+        $("#timerText").html(currentMinutes + ":" + currentSeconds) 
+        if(secs !== -1) setTimeout('Decrement()',1000);
+    }
