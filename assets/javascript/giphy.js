6b104b254277d492b09cf0a05ae03160d5b88353
jamesjkelly <jkellynjus@hotmail.com>
2017-09-18 11:59:35 -0400

    Update giphy.js

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -18,7 +18,7 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=0gWEFLh
           var defaultAnimatedSrc = results[i].images.fixed_height.url;
           var staticSrc = results[i].images.fixed_height_still.url;
           var image = $("<img>");
-          var para = $("<p>").text("rated: " + rating);
+          var para = $("<p>").text("rating: " + rating);
           image.attr("src", staticSrc);
           image.attr("data-state", "still");
           image.attr("data-still", staticSrc);
@@ -31,7 +31,37 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=0gWEFLh
   });
 }
   // on click display content
-  $(document).on("click", ".sport", giphy);
+  $(document).on("click", "button", giphy);
+  $('#addSports').on("mouseoff", function() {
 
-//  
+   $(this).attr('value', 'submit')
+
+  })
+  
+ 
+
+  $("#addSports").on("click", function(event) {
+        event.preventDefault();
+        var newSport = $("#sportsInput").val().trim();
+        topics.push(newSport);
+        $("#addSports").val('');
+        displayButtons();
+                $("#addSports").val('submit');
+
+      });
+
+  function displayButtons() {
+    $("#myButtons").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var a = $('<button class="btn btn-secondary">');
+      a.attr("id", "show");
+      a.attr("data-search", topics[i]);
+      a.text(topics[i]);
+      $("#myButtons").append(a);
+    }
+  }
+
+
+  displayButtons();
+ 
 });
